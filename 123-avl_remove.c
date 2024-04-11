5b5257600cec6867d6908c10111e7ebfda0d7fa9
eyob721 <eyobmelkamu721@gmail.com>
2024-04-11 17:48:41 +0300

    fix: betty errors

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -49,29 +49,6 @@ bst_t *_bst_remove(bst_t *root, int value)
 }
 
 /**
- * _height - a function that measures the height of sub tree of a node
- *           using recursion
- * @node: pointer to the root node of the tree
- * @level: current level in the tree
- *
- * Return: void
- */
-int __height(const binary_tree_t *node, size_t level)
-{
-	int height_left = 0, height_right = 0, height_max = 0;
-
-	if (node == NULL)
-		return (-1);
-	if (node->left == NULL && node->right == NULL)
-		return (level);
-	++level;
-	height_left = __height(node->left, level);
-	height_right = __height(node->right, level);
-	height_max = height_left > height_right ? height_left : height_right;
-	return (height_max);
-}
-
-/**
  * _get_avl_imbalance_node - a function that returns the imbalanced node from
  *                          an AVL tree
  * @tree: pointer to the root node of the tree

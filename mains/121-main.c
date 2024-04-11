fe81751542b97a17bf335b2ad557bd9c667cff9f
eyob721 <eyobmelkamu721@gmail.com>
2024-04-11 17:48:41 +0300

    feat: use a larger data

--- mains/121-main.c
+++ mains/121-main.c
@@ -3,38 +3,33 @@
 /**
  * main - Entry point
  *
- * Return: 0 on success, error code on failure
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	avl_t *root;
 	avl_t *node;
+	int array[] = {704, 309, 995, 739, 526, 294, 783, 645, 11,	775,
+				   689, 452, 390, 770, 189, 369, 300, 721, 891, 6,
+				   225, 298, 846, 549, 118, 278, 124, 867, 217, 468,
+				   804, 543, 876, 65,  179, 2,	 382, 568, 321, 768,
+				   56,	422, 586, 574, 533, 148, 707, 701, 715, 735};
+	size_t size = sizeof(array) / sizeof(array[0]);
+	size_t i;
 
 	root = NULL;
-	node = avl_insert(&root, 98);
-	printf("Inserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 402);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 12);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 46);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 128);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 256);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 512);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
-	node = avl_insert(&root, 50);
-	printf("\nInserted: %d\n", node->n);
-	binary_tree_print(root);
+	for (i = 0; i < size; i++)
+	{
+		node = avl_insert(&root, array[i]);
+		binary_tree_print(root);
+		if (node)
+			printf("Value inserted: %d\n", node->n);
+		else
+		{
+			printf("Value not inserted: %d\n", array[i]);
+		}
+		printf("\n");
+	}
 
 	binary_tree_delete(root);
 	return (0);
